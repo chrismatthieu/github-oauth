c2e5ab8485873827749276dacfaf3de95ee91e86
Chris Matthieu <chris@matthieu.us>
2011-07-09 19:34:34 -0700

    added overlay on chat

--- githubchat.rb
+++ githubchat.rb
@@ -6,8 +6,8 @@ require 'json'
   
 use Rack::Session::Cookie
 use OmniAuth::Builder do
-  provider :github, '74a1475d7f5564343099', 'f8310520720f56e931f086d519c435d800c6997a' #github chat
-  # provider :github, '25abed0273f2d33bd9a5', '5e1f21c1830c3c1a2816df2b1a1d798779763ce8' #testing
+  # provider :github, '74a1475d7f5564343099', 'f8310520720f56e931f086d519c435d800c6997a' #github chat
+  provider :github, '25abed0273f2d33bd9a5', '5e1f21c1830c3c1a2816df2b1a1d798779763ce8' #testing port 4567 or 9393
 end
 
 get '/' do
@@ -77,7 +77,133 @@ get '/issuesajax' do
 end
 
 
+get '/phono/:user/:repo/:id' do
+  user = params[:user]
+  repo = params[:repo]
+  id = params[:id]
+
+  html = '<html>
+  <head>
+   <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
+   <script type="text/javascript" src="http://s.phono.com/releases/0.2/jquery.phono.js"></script>
+
+   <script type="text/javascript">
+   			var phono;
+   			var call;
+   		    $(document).ready(function(){
+   		    	phono = $.phono({
+   		    	  apiKey: "f12dc371538cb43b6c594d",
+   		    	  onReady: function() {
+   					if( ! this.audio.permission() ){
+   					                this.audio.showPermissionBox();
+   		             }
+   					$("a.callme").addClass("phono-ready").text("Start");
+   		    	  },
+   				    phone: {
+   				  	onConnect: function(event) {
+   				  	},
+   				  	onDisconnect: function(event) {
+   				  		$(document).trigger("callHangUp");
+   				  	}
+   				  }
+
+   		    	});
+
+   		    	$("a.callme").click(function(){
+   		    		$(".digit-hldr").slideDown();
+   		    		$(this).hide();
+   		    		$("a.hangup").show().css("display","block");
+   		    		makeCall();
+   		    		return false;
+   		    	});
+
+   		    	$("a.hangup").click(function(){
+   		    		$(document).trigger("callHangUp");
+   		    		return false;
+   		    	});
+
+   		    	$(document).bind("callHangUp", function(){
+   		    		(call) ? call.hangup() : call = null;
+   		    		$(".digit-hldr").slideUp();
+   		    		$("a.hangup").hide();
+   		    		$("a.callme").show();
+   		    	});
+
+   		    })
+
+   		    function makeCall() {
+                   numberToDial = "app:9996134567";
+
+                   call = phono.phone.dial(numberToDial, {
+   					headers: [
+   					         {
+   					           name:"web",
+   					           value:"true"
+   					         },
+   							{
+   					           name:"code",
+   					           value: "' + user + repo + id + '"
+   					         }
+   					],
+   					tones: false,
+                       onAnswer: function(event) {
+
+                       },
+                       onHangup: function() {
+   						$(document).trigger("callHangUp");
+                       },
+                       onDisconnect: function() {
+   						$(document).trigger("callHangUp");
+                       }
+                   });
+               }
+
+   		</script>
+
+  </head>
+  <body>'
+
+  html << '<center><img src="/phono.png"><br/><div id="phono">
+  			<a class="callme" href="#">Loading...</a>
+  			<a class="hangup" href="#">hangup</a>
+  		</div></center></body></html>'
+  		
+  html
+  
+end
+
 get '/:user/:repo/issues/:id' do
+  user = params[:user]
+  repo = params[:repo]
+  id = params[:id]
+  
+  
+  html='<html id="mainHTML">
+  <head> 
+      <title>GitHub Chat</title>
+  	    <link href = "/style.css" type = text/css rel = stylesheet>
+
+  </head>
+  <body>
+  <div>
+      <iframe name="mainWebFrame" id="mainWebFrame" src="http://github.com/' + user + '/' + repo + '/issues/' + id + '" TITLE="GitHub" height="100%" width="100%" frameborder="0"
+              marginwidth="0" marginheight="0" vspace="0" hspace="0">
+      </iframe>
+  </div>
+  <div>
+      <iframe id="mainChatFrame" name="mainChatFrame" src="/phono/' + user + '/' + repo + '/' + id + '"
+              style="width: 300px; height: 250px; ">
+      </iframe>
+  </div>
+
+  </body>
+  </html>'
+  
+  html
+  
+end  
+
+get 'old/:user/:repo/issues/:id' do
   user = params[:user]
   repo = params[:repo]
   id = params[:id]
